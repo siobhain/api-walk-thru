364b73c5cbbb36e1dc34997a7faac8f0baebb5ec
siobhain <>
2023-06-01 21:21:27 +0000

    Add font files & POST request to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "VZn-Fx2ohpPGHAs_RhP_m6IgxYA";
+const API_KEY = "Lmc0CYZ2Y7Z2F9HauMEajw0IoxY";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
@@ -17,6 +17,15 @@ async function postForm(e) {
         body: form,
     });
 
+    const data = await response.json();
+
+    if (response.ok) {
+        // console.log(data);
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+
 }
 
 async function getStatus(e) {
@@ -33,6 +42,27 @@ async function getStatus(e) {
         throw new Error(data.error);
     }
 
+}
+
+function displayErrors(data) {
+
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 function displayStatus(data) {
